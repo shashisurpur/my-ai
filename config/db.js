77df3a9b8f3f82569e25995bfc676505da0c619e
Shashi Surpur <76897397+shashisurpur@users.noreply.github.com>
2025-03-22 00:47:46 +0530

    Add files via upload

---
+++ config/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+let cashed = global.mongoose || { conn: null, promise: null };
+
+export default async function connectDB() {
+  if (cashed.conn) return cashed.conn;
+  if (!cashed.promise) {
+    cashed.promise = mongoose
+      .connect(process.env.MONGODB_URI)
+      .then((mon) => mon);
+  }
+  try {
+    cashed.conn = await cashed.promise;
+  } catch (error) {
+    console.error("Error connecting to mongoDb", error);
+  }
+  return cashed.conn;
+}
