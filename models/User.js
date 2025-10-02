1045a5ce6623b7b508f122442ef9c3cdae378d3f
Shashi Surpur <shashisurpurv@gmail.com>
2025-10-02 23:44:53 +0530

    commented address for user

--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ const UserSchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true },
     image: { type: String, required: false },
-    address: { type: String, required: false },
+    // address: { type: String, required: false },
   },
   { timestamps: true }
 );
